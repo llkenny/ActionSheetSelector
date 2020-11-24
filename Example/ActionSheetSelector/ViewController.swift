f9ccd482bec1187fef5056712c0171f74203b525
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2020-11-24 09:10:42 +0300

    [5] Add option to display the selected item
    * Add selected parameter
    * Add tableView row selection if the parameter is provided
    * Update example

--- Example/ActionSheetSelector/ViewController.swift
+++ Example/ActionSheetSelector/ViewController.swift
@@ -15,7 +15,7 @@ final class ViewController: UIViewController {
         let image = UIImage(named: "checked")!
         let cellStyle = CellStyleConfig(titleSelectedColor: UIColor(red: 0, green: 0.37, blue: 0.72, alpha: 1), iconImage: image)
         let items = Array(1...5).map { Item(id: String($0), title: String($0)) }
-        ActionSheetSelectorViewController.present(parent: self, cellStyle: cellStyle, title: "Test", items: items) { item in
+        ActionSheetSelectorViewController.present(parent: self, cellStyle: cellStyle, title: "Test", items: items, selected: 1) { item in
             debugPrint(item)
         }
     }
